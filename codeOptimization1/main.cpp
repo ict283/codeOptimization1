648f2d7d7b4566658f26fe730db13d2c25722ab3
ict283 <glennlum@icloud.com>
2021-03-26 12:52:48 +0800

    Initial Commit

---
+++ codeOptimization1/main.cpp
@@ -0,0 +1,12 @@
+// For review purposes only! Does not represent the actual solution!
+
+#include <iostream>
+#include "toDoList.h"
+
+int main(void)
+{
+        ToDoList test;
+        test.run();
+
+        return 0;
+}
