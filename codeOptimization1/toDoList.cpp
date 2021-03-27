e57793f43fd110e22878122275ff85e1a93934c8
ict283 <glennlum@icloud.com>
2021-03-27 11:59:30 +0800

    Refactor code in implementation

--- codeOptimization1/toDoList.cpp
+++ codeOptimization1/toDoList.cpp
@@ -15,8 +15,16 @@ ToDoList::~ToDoList()
 void ToDoList::run()
 {
         store(read("to_do_data.txt"));
-        display();
-
+        
+        string dateInput; //eg."5.4.21"
+        string taskNoInput; //eg."3"
+        
+        cout<<"Enter date: ";
+        cin>>dateInput;
+        cout<<"Enter task no: ";
+        cin>>taskNoInput;
+         
+        display(dateInput,taskNoInput);
 }
 
 string ToDoList::read(const string fileName)
@@ -39,28 +47,43 @@ void ToDoList::store(string fileStreamData)
 {
         istringstream stringStream {fileStreamData}; //stringify
 
-        string item;
-        string taskDay="";
+        string item="";
+        string taskId="";
+        string taskDate="";
         int taskNum=1;
 
         while (getline(stringStream,item,','))
         {
                 if(item[0]=='#')
                 {
-                        taskDay= item.substr(1,item.length());
+                        taskDate= item.substr(1,item.length());
                         taskNum =1;
                 }
                 else
                 {
                         Task task;
-                        task.key = taskDay;
-                        task.num = taskNum++;
+                        task.taskId = taskDate+"."+to_string(taskNum++);
                         task.taskDescription = item;
+                        taskDatabase.insert(pair<string, Task>(task.taskId, task));
                 }
         }
 }
 
 void ToDoList::display()
 {
-        
+    map<string, Task> :: iterator itr;
+    
+    for(itr=taskDatabase.begin(); itr!=taskDatabase.end(); ++itr)
+    {
+        cout<<setw(10)<<setfill(' ')<<left;
+        cout<<(itr->first)<<" - "<<(itr->second).taskDescription<<endl; //first = key , second = value
+    }
+}
+
+void ToDoList::display(string date, string taskNo)
+{
+    string taskId = date+"."+taskNo;
+    map<string,Task>:: const_iterator itr = taskDatabase.find(taskId);
+    cout<<setw(10)<<setfill(' ')<<left;
+    cout<<(itr->first)<<" - "<<(itr->second).taskDescription<<endl;
 }
